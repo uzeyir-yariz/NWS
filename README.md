df12b8742ee50de1caca263d7dc0dc6957e36dc0
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2023-09-09 22:06:21 +0300

    extremly hight update

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # NWS
 
-Next_Web_Solutions
+> Next_Web_Solutions
 
-run the css converter
+## kullanılan özellikler
+
+### kullanılan diller
+
+- html
+- css (scss converter)
+- scss (scss converter)
+- JS
+
+### kullanılan teknolojiler
+
+- Jquery 
+- npm scss converter
+- JS opject
+- JS animation
+- scss varibles
+
+## run the css converter
 
     npm run scss
--- public/src/javascript/simple_js.js
+++ public/src/javascript/simple_js.js
@@ -1,23 +1,5 @@
-const overlay = document.querySelector('.overlay');
-const navbar = document.querySelector('.navbar');
-
 // ! burası değiştirlimesi gerekebilir
-function show_eror(){
-    alert("yapım aşamasında");
-}
+function show_eror(){alert("yapım aşamasında");}
 
-// * overlay ayarları
-function close_nav(){
-    overlay.style.width = "0%"
-} function open_nav(){
-    overlay.style.width = "100%"
-}
+// * burada ki overlay adlı kod parçası nav.html adlı yere taşındı 
 
-// * scroled ayarları
-function scroll_navbar(){
-    const scrollY = window.scrollY;
-    const shouldApplyStyles = scrollY > 0;
-
-    navbar.style.backgroundColor = shouldApplyStyles ? '#272727be' : 'transparent';
-
-} window.addEventListener('scroll', scroll_navbar);
