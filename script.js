7b626d0bb75a38e820b891f8b79b981a21a45d9b
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2023-09-25 21:34:06 +0300

    update to work

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const firebase = require("firebase/app");
-require("firebase/firestore");
-const fs = require("fs");
+const fetch = require('node-fetch');
 
-// Firebase projenizin yapılandırma bilgilerini buraya ekleyin
 const firebaseConfig = {
     apiKey: "AIzaSyCvt9j5NilGWFl2C2Y67uKhuhKCKS_OeeA",
     authDomain: "nws-8b76d.firebaseapp.com",
@@ -11,34 +8,18 @@ const firebaseConfig = {
     messagingSenderId: "461109278827",
     appId: "1:461109278827:web:4a03a1fbf5e2b30dc6192e",
     measurementId: "G-2YS02K1RPB"
-};
-// Firestore referansını alın
-const db = firebase.initializeApp(firebaseConfig).firestore();
+  };
 
-// Firestore'dan verileri alın
-const collectionName = "products"; // Koleksiyon adını buraya ekleyin
+const collectionName = 'your-collection-name'; // Koleksiyon adını buraya ekleyin
+const apiKey = firebaseConfig.apiKey;
 
-const getData = async () => {
-  try {
-    const snapshot = await db.collection(collectionName).get();
-    const data = [];
+const firestoreUrl = `https://firestore.googleapis.com/v1/projects/${firebaseConfig.projectId}/databases/(default)/documents/${collectionName}?key=${apiKey}`;
 
-    snapshot.forEach((doc) => {
-      data.push(doc.data());
-    });
-
-    // Firestore verilerini JSON formatına dönüştürün
-    const jsonData = JSON.stringify(data, null, 2);
-
-    // JSON verilerini bir dosyaya yazın
-    fs.writeFileSync("data.json", jsonData, "utf-8");
-    console.log("Veriler JSON dosyasına kaydedildi.");
-  } catch (error) {
-    console.error("Veriler alınamadı: ", error);
-  } finally {
-    // Firebase bağlantısını kapatın
-    firebase.app().delete();
-  }
-};
-
-getData();
+fetch(firestoreUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+  })
+  .catch((error) => {
+    console.error('Veriler alınamadı:', error);
+  });
