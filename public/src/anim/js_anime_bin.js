16bf6f3287a3d2394cf0f7d27b5e2094bb9fd898
uzeyir-yariz <uzeyiryariz13@gmail.com>
2023-10-18 18:13:28 +0300

    update to main page

---
+++ public/src/anim/js_anime_bin.js
@@ -0,0 +1,23 @@
+// Rastgele bir 1 ve 0 dizisi oluştur
+function generateRandomBinaryString(length) {
+    let binaryString = '';
+    for (let i = 0; i < length; i++) {
+        const randomBit = Math.random() < 0.5 ? '0' : '1';
+        binaryString += randomBit;
+    }
+    return binaryString;
+}
+
+// Sayfa yüklendiğinde çalışacak fonksiyon
+window.addEventListener('load', () => {
+    const container = document.querySelector('.column_container'); 
+    const screenWidth = window.innerWidth; // Ekranın genişliği
+
+    for (let i = 0; i < 25; i++) {
+        const div = document.createElement('div');
+        const randomBinaryString = generateRandomBinaryString(screenWidth / 10);
+        div.textContent = randomBinaryString;
+        div.classList = 'random_column';
+        container.appendChild(div);
+    }
+});
