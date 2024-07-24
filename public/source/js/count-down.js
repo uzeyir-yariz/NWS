d457b060958b3aef377632b2f4852d197e2c90c8
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-24 14:35:14 +0300

    mobile-responsive

---
+++ public/source/js/count-down.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var toDayFromNow =
+      new Date("Dec 31, 2024 23:59:59").getTime() / 1000 +
+      3600 / 60 / 60 / 24 -
+      1;
+    var flipdown = new FlipDown(toDayFromNow).start().ifEnded(() => {
+      document.querySelector(
+        ".flipdown"
+      ).innerHTML = `<h2>zaman doldu çık dışarı</h2>`;
+    });
+  });
