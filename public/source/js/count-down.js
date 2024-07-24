f3be0a2d437d6c03a1a3c594e46b63500aea99c4
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-24 14:59:37 +0300

    last update

--- public/source/js/count-down.js
+++ public/source/js/count-down.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     var toDayFromNow =
-      new Date("Dec 31, 2024 23:59:59").getTime() / 1000 +
+      new Date("Aug 31, 2024 23:59:59").getTime() / 1000 +
       3600 / 60 / 60 / 24 -
       1;
     var flipdown = new FlipDown(toDayFromNow).start().ifEnded(() => {
