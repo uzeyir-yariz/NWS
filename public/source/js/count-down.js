01cc5ef13da04ca9bd43d540b7f80e4e6a67acaa
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-08-19 12:38:57 +0300

    "Updated Firebase configuration files, refactored HTML and CSS code, removed rule.txt file, and modified JavaScript countdown script."

--- public/source/js/count-down.js
+++ public/source/js/count-down.js
@@ -1,11 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var toDayFromNow =
-      new Date("Aug 31, 2024 23:59:59").getTime() / 1000 +
-      3600 / 60 / 60 / 24 -
-      1;
-    var flipdown = new FlipDown(toDayFromNow).start().ifEnded(() => {
-      document.querySelector(
-        ".flipdown"
-      ).innerHTML = `<h2>zaman doldu çık dışarı</h2>`;
-    });
-  });
+const count_down = document.getElementById("count-down");
+const countDownDate = new Date("Sept 30, 2024 00:00:00").getTime(); // tarih
+
+const x = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  const write = [days, hours, minutes, seconds].map(i => i.toString().padStart(2, "0")) // yanına sıfır koymak için kullandığım kod
+  count_down.innerHTML = "";
+  
+  write.forEach((i) => {
+    const div = document.createElement("div");
+    div.textContent = i;
+    count_down.appendChild(div);
+  })
+
+  if (distance < 0) {
+    clearInterval(x);
+    count_down.innerHTML = "<h2>zaman doldu!</h2>"
+  }
+}, 1000);
