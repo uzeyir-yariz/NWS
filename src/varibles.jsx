163d7b44b641f065f60ecbfe859f4ac9a7523ecb
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-11-02 08:50:39 +0300

    "Updated environment variable names from VITE_ to VERCEL_ in varibles.jsx"

--- src/varibles.jsx
+++ src/varibles.jsx
@@ -1,4 +1,4 @@
-export const nws_master_key = import.meta.env.VITE_NWS_DATA_MASTER_KEY;
-export const nws_access_key = import.meta.env.VITE_NWS_DATA_ACCESS_KEY;
-export const nws_data_url = import.meta.env.VITE_NWS_DATA_URL;
+export const nws_master_key = import.meta.env.VERCEL_NWS_DATA_MASTER_KEY;
+export const nws_access_key = import.meta.env.VERCEL_NWS_DATA_ACCESS_KEY;
+export const nws_data_url = import.meta.env.VERCEL_NWS_DATA_URL;
 
