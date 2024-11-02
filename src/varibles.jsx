57ad9d33968c2e0bfe9813164d1b7a685552c7d3
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-11-02 09:42:53 +0300

    change

--- src/varibles.jsx
+++ src/varibles.jsx
@@ -1,5 +1,5 @@
-export const nws_master_key = import.meta.env.VITE_NWS_DATA_MASTER_KEY;
-export const nws_access_key = import.meta.env.VITE_NWS_DATA_ACCESS_KEY;
-export const nws_data_url = import.meta.env.VITE_NWS_DATA_URL;
+export const nws_master_key = import.meta.env.NWS_DATA_MASTER_KEY;
+export const nws_access_key = import.meta.env.NWS_DATA_ACCESS_KEY;
+export const nws_data_url = import.meta.env.NWS_DATA_URL;
 
-console.log(import.meta.env.VITE_NWS_DATA_MASTER_KEY)
+console.log(import.meta.env.NWS_DATA_MASTER_KEY)
